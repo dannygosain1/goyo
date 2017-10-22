2514bbfabdc25bb1c98f37c5c8b930e8e3396073
sophia <scastellarin95@gmail.com>
2017-10-22 14:47:06 -0400

    Start android demo
    Shows the accelerometer readings in the x, y and z directions

---
+++ AndroidDemo/crap/app/src/main/java/om/poop/crap/MainActivity.java
@@ -0,0 +1,91 @@
+package om.poop.crap;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private boolean color = false;
+    private View view;
+    private long lastUpdate = 0;
+    private long lastx = 0;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = findViewById(R.id.textView);
+        view.setBackgroundColor(Color.WHITE);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
+        }
+
+    }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        TextView text = (TextView)findViewById(R.id.text2);
+        String x_str = "x: " + x;
+        text.setText(x_str);
+
+        text = (TextView)findViewById(R.id.text3);
+        String y_str = "y: " + y;
+        text.setText(y_str);
+
+        text = (TextView)findViewById(R.id.text4);
+        String z_str = "z: " + z;
+        text.setText(z_str);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and
+        // accelerometer sensors
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
