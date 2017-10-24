22d037e3018d12e71dc10fb6946eb515192101a9
dannygosain1 <dgosain@uwaterloo.ca>
2017-10-23 20:57:48 -0400

    DG/RS - worked on saving the data in a csv

--- iPhoneDemo/ViewController.swift
+++ iPhoneDemo/ViewController.swift
@@ -12,27 +12,57 @@ import CoreMotion
 class ViewController: UIViewController {
     
     var motionManager = CMMotionManager()
-
+//    var xrawData: [String] = []
+//    var yrawData: [String] = []
+//    var zrawData: [Double] = []
+    var timestamprawData = [String]()
+    var fileName = "rawData.csv"
+    var path: URL?
+    var csvText = "x,y,z,timeStamp\n"
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func viewDidAppear(_ animated: Bool) {
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    func playButtonTapped() {
         motionManager.accelerometerUpdateInterval = 0.2 // update data interval
-        
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
             if let myData = data
             {
                 print(myData)
+                let newLine = "\(myData.acceleration.x),\(myData.acceleration.y),\(myData.acceleration.z),\(myData.timestamp)\n"
+                self.csvText.append(newLine)
             }
         }
+
+    }
+    
+    func pauseButtonTapped() {
+        motionManager.stopAccelerometerUpdates()
+        do {
+            //try FileManager.default.createFile(atPath: self.path!.absoluteString, contents: csvText)
+            try csvText.write(to: self.path!, atomically: true, encoding: String.Encoding.utf8)
+
+        } catch {
+            print("Failed to create file")
+            print("\(error)")
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        let documentsDirectory =  NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        self.path =  NSURL(fileURLWithPath: documentsDirectory).appendingPathComponent(self.fileName)
+        playButton.addTarget(self, action: #selector(ViewController.playButtonTapped), for: .touchUpInside)
+        pauseButton.addTarget(self, action: #selector(ViewController.pauseButtonTapped), for: .touchUpInside)
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
