c9b24c444f64a1ca2c186fd448a652de46815b0a
sophia <scastellarin95@gmail.com>
2017-11-03 23:25:53 -0400

    Data

--- tools/collect_arduino_fsr_data.py
+++ tools/collect_arduino_fsr_data.py
@@ -19,17 +19,20 @@ def main(args):
     collecting = True
 
     with open('data.csv', 'wb') as csvfile:
-        fieldnames = ['timestamp', 'fsr_reading']
+        fieldnames = ['timestamp', 'fsr0', 'fsr1', 'fsr2', 'fsr3']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',', quoting=csv.QUOTE_MINIMAL)
             
         writer.writeheader()
         while collecting: 
-            dp = data_feed.readline() 
-            time_stamp = datetime.datetime.fromtimestamp(
-                time.time()).strftime("%Y-%m-%dT%H:%M:%S")
-            writer.writerow(
-                {'timestamp': time_stamp, 'fsr_reading': dp.strip()
-            })
+            dp = data_feed.readline().split(" ")
+            if len(dp) == 5: 
+                time_stamp = datetime.datetime.fromtimestamp(
+                    time.time()).strftime("%Y-%m-%dT%H:%M:%S")
+                writer.writerow(
+                    {'timestamp': time_stamp, 
+                    'fsr0': dp[1], 'fsr1': dp[2], 
+                    'fsr2': dp[3], 'fsr3': dp[4].strip()
+                })
         
             if time.time() > start_time + duration:
                 endtime = time.time()
@@ -43,7 +46,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Collect data on serial port")
     parser.add_argument('--port', nargs=1, default=["/dev/ttyACM0"], type=str,  dest="port", help='selects the port') 
-    parser.add_argument('--duration', nargs=1, default=[1], type=int, dest="duration", help="duration of data collection") 
+    parser.add_argument('--duration', nargs=1, default=[65], type=int, dest="duration", help="duration of data collection") 
     args = parser.parse_args()
 
     main(args)
