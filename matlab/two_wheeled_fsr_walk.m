bed525a67df86305e3db6439ba7a27ee886677c5
Sophia <scastellarin95@gmail.com>
2017-11-07 15:21:47 -0500

    Plot a bunch of fsr data in time domain

--- matlab/two_wheeled_fsr_walk.m
+++ matlab/two_wheeled_fsr_walk.m
@@ -3,5 +3,5 @@ fsr3 = csvread('two-wheels\data_force_4_axis_test3.csv', 1, 1);
 fsr4 = csvread('two-wheels\data_force_4_axis_test4.csv', 1, 1);
 
 PlotFSRData(fsr2, 1, 0)
-% PlotFSRData(fsr3)
-% PlotFSRData(fsr4)
+PlotFSRData(fsr3, 1, 0)
+PlotFSRData(fsr4, 1, 0)
