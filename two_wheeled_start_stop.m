3dfb1c3cb278dad2e445a9df297cdfbeca5b91ee
Sophia Castellarin <scastellarin@continuum.io>
2017-11-13 17:03:51 -0500

    Delete two_wheeled_start_stop.m

--- two_wheeled_start_stop.m
+++
@@ -1,7 +0,0 @@
-twwalker1 = csvread('two-wheels\walker-1.csv', 1);
-twwalker2 = csvread('two-wheels\walker-2.csv', 1);
-twwalker3 = csvread('two-wheels\walker-3.csv', 1);
-
-twwalker1_start_to_stop =  twwalker1(120:300, :);
-
-AccelerometerAnalysis(twwalker1_start_to_stop);
