098bc9237c9e75982038b49939f63b792f20ff28
Sophia Castellarin <scastellarin@continuum.io>
2017-11-16 14:14:38 -0500

    Extract features from all x,y,z dir

--- matlab/ExtractFeatures.m
+++ matlab/ExtractFeatures.m
@@ -1,17 +1,27 @@
 % Script that takes data and extracts all the signal features
 
 twwalker1 = csvread('two-wheels\walker-1.csv', 1);
+x_accel = twwalker1(:,1);
+y_accel = twwalker1(:,2);
 z_accel = twwalker1(:,3);
 
-num_features = 3;
+num_features = 9;
 rate = 10;          % Hz
 window_size = 2;    % sec
 window_overlap = 1; %sec
-windows = WindowCreation(z_accel, rate, window_size, window_overlap);
 
-feature_data = zeros(length(windows), num_features);
+xwindows = WindowCreation(x_accel, rate, window_size, window_overlap);
+ywindows = WindowCreation(y_accel, rate, window_size, window_overlap);
+zwindows = WindowCreation(z_accel, rate, window_size, window_overlap);
 
-for k=1:length(windows)
-    cur_window = windows{k};
-    feature_data(k,:) = ExtractSignalFeatures(cur_window);
+feature_data = zeros(length(zwindows), num_features);
+
+for k=1:length(zwindows)
+    xcur_window = xwindows{k};
+    ycur_window = ywindows{k};
+    zcur_window = zwindows{k};
+    
+    feature_data(k,1:3) = ExtractSignalFeatures(xcur_window);
+    feature_data(k,4:6) = ExtractSignalFeatures(ycur_window);
+    feature_data(k,7:9) = ExtractSignalFeatures(zcur_window);
 end
