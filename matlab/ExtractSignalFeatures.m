cf7f13178472d8f107a95d3f252c9912f6e3f10c
Sophia Castellarin <scastellarin@continuum.io>
2017-11-16 21:33:21 -0500

    Extract the power of the signal as well

--- matlab/ExtractSignalFeatures.m
+++ matlab/ExtractSignalFeatures.m
@@ -1,13 +1,15 @@
 function [ feature_vector ] = ExtractSignalFeatures( sample )
 % ExtractSignalFeatures Returns a feature vector with characteristics of interest 
 %   Given a Nx1 vector representing accelerometer data, this function will
-%   return a feature vector of the form [mean, variance, max]
+%   return a feature vector of the form [mean, variance, energy]
 
+
+    sample_fourier = fft(sample);
+    
     sample_mean = mean(sample);
     sample_var = var(sample);
-    sample_max = max(abs(sample));
+    sample_energy = sum(sample_fourier.*conj(sample_fourier));
     
-    feature_vector = [sample_mean sample_var, sample_max];
+    feature_vector = [sample_mean sample_var, sample_energy];
 
 end
-
