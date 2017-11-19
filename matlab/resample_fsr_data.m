783d217e4a1739378a93c5af656763fcba31caaf
Sophia <scastellarin95@gmail.com>
2017-11-19 16:53:26 -0500

    Add resampled fsr data

--- matlab/resample_fsr_data.m
+++ matlab/resample_fsr_data.m
@@ -1,4 +1,4 @@
 %resamples acceleration data and writes it to different files
-orig = csvread('two-wheels-second-data\FSR\data_walk6.csv', 1, 1);
+orig = csvread('two-wheels-second-data\FSR\data_walk4.csv', 1, 1);
 resampled = resample(orig, 24, 10);
-csvwrite('two-wheels-second-data\FSR\data_walk6_resampled.csv', resampled)
+csvwrite('two-wheels-second-data\FSR\data_walk4_resampled.csv', resampled)
