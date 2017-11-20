df4ea9d14f511dc2367234ea92b57031cd847888
RaunaqSuri <raunaq.suri@hotmail.com>
2017-11-20 16:03:20 -0500

    added walk3 data

--- matlab/RawFeaturesToStandardized.m
+++ matlab/RawFeaturesToStandardized.m
@@ -1,16 +1,16 @@
-walk6corner_raw = csvread('feature-vectors\walk6corner_raw.csv', 1);
-walk6top_raw = csvread('feature-vectors\walk6top_raw.csv', 1);
-walk6side_raw = csvread('feature-vectors\walk6side_raw.csv', 1);
-walk6front_raw = csvread('feature-vectors\walk6front_raw.csv', 1);
+walkcorner_raw = csvread('feature-vectors\walk3corner_raw.csv', 1);
+walktop_raw = csvread('feature-vectors\walk3top_raw.csv', 1);
+walkside_raw = csvread('feature-vectors\walk3side_raw.csv', 1);
+walkfront_raw = csvread('feature-vectors\walk3front_raw.csv', 1);
 
 %all the classifications are the same for all of them
 
-classifications = walk6corner_raw(:,1);
+classifications = walkcorner_raw(:,1);
 
-standard_corner = StandardizeFeatures(walk6corner_raw(:,2:12));
-standard_top = StandardizeFeatures(walk6top_raw(:,2:12));
-standard_side = StandardizeFeatures(walk6side_raw(:,2:12));
-standard_front = StandardizeFeatures(walk6corner_raw(:,2:12));
+standard_corner = StandardizeFeatures(walkcorner_raw(:,2:12));
+standard_top = StandardizeFeatures(walktop_raw(:,2:12));
+standard_side = StandardizeFeatures(walkside_raw(:,2:12));
+standard_front = StandardizeFeatures(walkcorner_raw(:,2:12));
 
 
 csvwrite('feature-vectors\walk6corner_standard.csv', horzcat(classifications, standard_corner))
--- model/model.py
+++ model/model.py
@@ -1,13 +1,16 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-walk6csv = np.loadtxt ('feature_vectors/walk6front_raw.csv', delimiter=",", skiprows=1)
+walk6csv = np.loadtxt ('feature_vectors/walk6top_raw.csv', delimiter=",", skiprows=1)
+walk3csv = np.loadtxt ('feature_vectors/walk3top_raw.csv', delimiter=",", skiprows=1)
 walk6_target = walk6csv[:, 0]
 walk6_data = walk6csv[:, 1:]
+walk3_target = walk3csv[:, 0]
+walk3_data = walk3csv[:, 1:]
 
-data_train, data_test, target_train, target_test = train_test_split(walk6_data, walk6_target, test_size = 0.4)
+#data_train, data_test, target_train, target_test = train_test_split(walk6_data, walk6_target, test_size = 0.4)
 
 gnb = GaussianNB()
 
-walk_pred = gnb.fit(data_train, target_train).predict(data_test)
-print("Number of mislabeled points out of a total %d points : %d" % (data_test.shape[0],(target_test != walk_pred).sum()))
+walk_pred = gnb.fit(walk6_data, walk6_target).predict(walk3_data)
+print("Number of mislabeled points out of a total %d points : %d" % (walk3_data.shape[0],(walk3_target != walk_pred).sum()))
