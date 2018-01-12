3cbd40447eefc5b90c38cb9c85041067aa182bc2
sophia <scastellarin95@gmail.com>
2018-01-12 11:34:18 -0500

    Change rolling window

--- dip/ExtractFeatures.py
+++ dip/ExtractFeatures.py
@@ -17,21 +17,22 @@ def extract_windowed_features(data, window_size, sample_freq):
     sample_mode = lambda a: mode(a)[0]
     sample_bandpower = lambda a: bandpower(a, sample_freq, a.min(), a.max())
     data_win = data.rolling(window_size)
+    sample_win = int(window_size/2)
 
-    # NOTE:[window_size-1::window_size] extracts the last element off the rolling window
+    # NOTE:[sample_win::sample_win+1] extracts the last element off the rolling window
     data_features = {
-        'x_mean': data_win.top_x.mean()[window_size-1::window_size],
-        'y_mean': data_win.top_y.mean()[window_size-1::window_size],
-        'z_mean': data_win.top_z.mean()[window_size-1::window_size],
-        'x_var': data_win.top_x.var()[window_size-1::window_size],
-        'y_var': data_win.top_y.var()[window_size-1::window_size],
-        'z_var': data_win.top_z.var()[window_size-1::window_size],
-        'x_energy': data_win.top_x.apply(sample_bandpower)[window_size-1::window_size],
-        'z_energy': data_win.top_z.apply(sample_bandpower)[window_size-1::window_size],
-        'y_energy': data_win.top_y.apply(sample_bandpower)[window_size-1::window_size],
-        'fsr1_med': data_win.fsr1.median()[window_size-1::window_size],
-        'fsr2_med': data_win.fsr2.median()[window_size-1::window_size],
-        'is_walking': data_win.is_walking.apply(sample_mode)[window_size-1::window_size]
+        'x_mean': data_win.top_x.mean()[sample_win::sample_win+1],
+        'y_mean': data_win.top_y.mean()[sample_win::sample_win+1],
+        'z_mean': data_win.top_z.mean()[sample_win::sample_win+1],
+        'x_var': data_win.top_x.var()[sample_win::sample_win+1],
+        'y_var': data_win.top_y.var()[sample_win::sample_win+1],
+        'z_var': data_win.top_z.var()[sample_win::sample_win+1],
+        'x_energy': data_win.top_x.apply(sample_bandpower)[sample_win::sample_win+1],
+        'z_energy': data_win.top_z.apply(sample_bandpower)[sample_win::sample_win+1],
+        'y_energy': data_win.top_y.apply(sample_bandpower)[sample_win::sample_win+1],
+        'fsr1_med': data_win.fsr1.median()[sample_win::sample_win+1],
+        'fsr2_med': data_win.fsr2.median()[sample_win::sample_win+1],
+        'is_walking': data_win.is_walking.apply(sample_mode)[sample_win::sample_win+1]
     }
     return pandas.DataFrame(data=data_features)
 
@@ -42,7 +43,7 @@ def main(args):
     fsr_data = [raw_data.fsr1, raw_data.fsr2]
     accelerometer_data = [raw_data.top_x, raw_data.top_y, raw_data.top_z]
     rate = 24                   # Hz
-    window_size_sec = 1         # sec
+    window_size_sec = 2         # sec
     window_size = rate*window_size_sec
     features = extract_windowed_features(raw_data, window_size, rate)
     features.to_csv('extracted_features.csv')
