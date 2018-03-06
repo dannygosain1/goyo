29810f43a20c0a5121c21f2d3e544d8879d710d7
Danny Gosain <dgosain@uwaterloo.ca>
2018-03-06 06:05:01 -0500

    completed homepage

--- application/FSCalendarSwiftExample/HomeViewController.swift
+++ application/FSCalendarSwiftExample/HomeViewController.swift
@@ -9,27 +9,53 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var dailyGoal: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var starCosmos: CosmosView!
+    @IBOutlet weak var message: UILabel!
+    
+    var goal = 60// to be provided
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        dailyGoal.text = "Daily Goal: " + String(goal) + " minutes"
+        
+        var rating = 4.0 // var type double
+        let activityLeft = (5-rating)/5 * 60
+        
+        dailyGoal.text = "Daily Goal: " + String(Int(goal)) + " active minutes"
+        
+        
+        if (rating <= 1.5) {
+            image.image = UIImage(named: "old-man-sad.png")
+            message.text = "Let's get going, you still have " + String(Int(activityLeft)) + " active minutes left."
+        } else if (rating > 1.5 && rating < 4) {
+            image.image = UIImage(named: "old-man-happy.png")
+            message.text = "Keep up the good work, you only have " + String(Int(activityLeft)) + " active minutes left."
+        } else if (rating >= 4 && rating < 5){
+            image.image = UIImage(named: "old-man-very-happy.png")
+            message.text = "You're ALMOST there, only " + String(Int(activityLeft)) + " active minutes left."
+        } else if (rating == 5) {
+            image.image = UIImage(named: "old-man-very-happy.png")
+            message.text = "Congratulations on the 5 stars, you have reached your daily activity goal."
+        } else {
+            image.image = UIImage(named: "old-man-happy.png")
+            message.text = " "
+        }
+        
+        starCosmos.rating = rating
+        starCosmos.settings.updateOnTouch = false
+        
+        message.numberOfLines = 0
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
